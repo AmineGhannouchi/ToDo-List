667c00f0670f550e32e1be0675a1408f5558c5cd
amine ghannouchi <amineghannouchi111@gmail.com>
2025-02-05 22:18:46 +0100

    project done

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -15,9 +15,27 @@ function ToDoList() {
         const updatedToDo = todo.filter((_, i) => i !== index);
         setToDo(updatedToDo);
     }
-    function handelUpToDo(index) {}
-    function handelDownToDo(index) {}
-    function handelStatusToDo() {}
+    function handelUpToDo(index) {
+        if (index === 0) return;
+        [todo[index],todo[index-1]] = [todo[index-1],todo[index]];
+        setToDo([...todo]);
+    }
+    function handelDownToDo(index) {
+        if (index === todo.length - 1) return;
+        [todo[index],todo[index+1]] = [todo[index+1],todo[index]];
+        setToDo([...todo]);
+
+    }
+    function handelStatusToDo(index) {
+        todo[index].status = !todo[index].status;
+        setToDo([...todo]);
+        if (todo[index].status) {
+            document.getElementsByClassName("v"+index)[0].style.textDecoration = "line-through";
+        }else{
+            document.getElementsByClassName("v"+index)[0].style.textDecoration = "none";
+        }
+
+    }
     return (
         <>
         <input type="text" onChange={(e) => setTask(e.target.value)} />
@@ -25,12 +43,12 @@ function ToDoList() {
         
         <ul>
             {todo.map((item,index) => (
-                <li key={index}>
+                <li className={"v"+index} key={index}>
                 {item.task}
                 <button onClick={()=>handelDeleteToDo(index)}>Delete</button>
                 <button onClick={()=>handelUpToDo(index)}>Up</button>
                 <button onClick={()=>handelDownToDo(index)}>Down</button>
-                <button onClick={handelStatusToDo}>Status</button>
+                <button onClick={()=>handelStatusToDo(index)}>Status</button>
                 </li>
             ))}
         </ul>
