24e30f76f646dfc4b501d4b17c7e53370d4527cd
amine ghannouchi <amineghannouchi111@gmail.com>
2025-02-05 21:52:22 +0100

    handleAddToDo done

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+
+function ToDoList() {
+    const [todo, setToDo] = useState([]);
+    const [task, setTask] = useState("");
+
+    function handleAddToDo() {
+        if (task.trim() != "") {
+            const todos = {task:task,status: false};
+            setToDo(prevToDo =>[...prevToDo,todos]);
+            console.log(todo);
+        }
+    }
+    function handelDeleteToDo() {}
+    function handelUpToDo() {}
+    function handelDownToDo() {}
+    function handelStatusToDo() {}
+    return (
+        <>
+        <input type="text" onChange={(e) => setTask(e.target.value)} />
+        <button onClick={handleAddToDo}>Add</button>
+        
+        <ul>
+            {todo.map((item,index) => (
+                <li key={index}>
+                {item.task}
+                <button onClick={handelDeleteToDo}>Delete</button>
+                <button onClick={handelUpToDo}>Up</button>
+                <button onClick={handelDownToDo}>Down</button>
+                <button onClick={handelStatusToDo}>Status</button>
+                </li>
+            ))}
+        </ul>
+        </>
+    );
+}
+export default ToDoList;
