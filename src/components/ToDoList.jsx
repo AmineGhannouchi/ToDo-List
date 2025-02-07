ce6e8b8ccc1de1fc222f3a0751efc230e0c0f787
amine ghannouchi <amineghannouchi111@gmail.com>
2025-02-07 19:45:46 +0100

    handelEditToDo

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -24,8 +24,32 @@ function ToDoList() {
         if (index === todo.length - 1) return;
         [todo[index],todo[index+1]] = [todo[index+1],todo[index]];
         setToDo([...todo]);
-
     }
+
+    function handelEditToDo(index) {
+        //create an input to change emement
+        const input = document.createElement("input");
+        const p = document.getElementsByClassName("v"+index)[0].children[0];
+        document.getElementsByClassName("v"+index)[0].removeChild(p);
+        input.value = todo[index].task;
+        document.getElementsByClassName("v"+index)[0].insertBefore(input, document.getElementsByClassName("v"+index)[0].firstChild);
+
+        input.focus();
+        input.onblur = () => {
+            todo[index].task = input.value;
+            setToDo([...todo]);
+            //remove input
+            document.getElementsByClassName("v"+index)[0].removeChild(input);
+            //adding a first child in classname "v"+index with value "task"
+            const p = document.createElement("p");
+            p.innerHTML = todo[index].task;
+            //adding p in first place of class "v"+index with value of task
+            document.getElementsByClassName("v"+index)[0].insertBefore(p, document.getElementsByClassName("v"+index)[0].firstChild);
+
+        }
+    }
+
+
     function handelStatusToDo(index) {
         todo[index].status = !todo[index].status;
         setToDo([...todo]);
@@ -53,10 +77,13 @@ function ToDoList() {
             </tr>
             {todo.map((item,index) => (
                 <tr className={"v"+index} key={index}>
-                    <td>{item.task}</td>
+                    <td>
+                        <p>{item.task}</p>
+                    </td>
                     <td><button onClick={()=>handelDeleteToDo(index)}>🚮</button></td>
                     <td><button onClick={()=>handelUpToDo(index)}>👆</button></td>
                     <td><button onClick={()=>handelDownToDo(index)}>👇</button></td>
+                    <td><button onClick={()=>handelEditToDo(index)}>📝</button></td>
                     <td><button id={"id"+index} onClick={()=>handelStatusToDo(index)}>✅</button></td>
                 </tr>
             ))}
